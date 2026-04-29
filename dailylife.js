5c1482ab0c11e56392bdf51e5df89e6da3efe0b9
lionelofficial9 <lionelkirenga7@gmail.com>
2026-04-29 15:39:01 +0300

    Add files via upload

---
+++ dailylife.js
@@ -0,0 +1,22 @@
+// Part 1: Variables and Data Types
+let name = "Lionel";
+let age = 22;
+let favoriteFood = "Brochettes";
+let studyHoursPerDay = 4;
+let isStudent = true; // Bonus
+
+// Part 2: Simple Calculations
+let studyHoursPerWeek = studyHoursPerDay * 7;
+let ageInMonths = age * 12;
+let dogYears = age * 7; // Bonus
+
+// Part 3: Output
+console.log("Hello, my name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I love eating " + favoriteFood + ".");
+console.log("I study " + studyHoursPerDay + " hours per day, which is " + studyHoursPerWeek + " hours per week.");
+console.log("I am " + ageInMonths + " months old.");
+
+// Bonus Output
+console.log("In dog years, I am " + dogYears + " years old.");
+console.log("Is student: " + isStudent);
